0348ff141de7baea98c0d1a5f29b40615d52db70
Lucas Korz <lucaskorz10@gmail.com>
2026-02-16 10:27:09 -0300

    feat: added expectation

--- __tests__/useCases/PlaceOrder.spec.ts
+++ __tests__/useCases/PlaceOrder.spec.ts
@@ -3,7 +3,7 @@ import type { IEmailGateway } from '@/interfaces/gateways/IEmailGateway.js';
 import type { IQueueGateway } from '@/interfaces/gateways/IQueueGateway.js';
 import type { IOrdersRepository } from '@/interfaces/repositories/IOrdersRepository.js'
 import { PlaceOrder } from '@/useCases/PlaceOrder.js'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { mock } from 'vitest-mock-extended'
 
 const ordersRepositoryMock = mock<IOrdersRepository>();
@@ -27,5 +27,6 @@ describe('PlaceOrder', () => {
     expect(queueGatewayMock.publishMessage).toHaveBeenCalledWith({ orderId: expect.any(String) });
 
     expect(emailGatewayMock.sendEmail).toHaveBeenCalledTimes(1);
+    expect(emailGatewayMock.sendEmail).toHaveBeenCalledWith(expect.any(Object))
   })
 })
